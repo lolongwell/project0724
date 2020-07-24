8d89a9a3fc7e0b6a6809e6341ae34e6404fe0b97
lolongwell <lolong.well@qq.com>
2020-07-25 00:22:19 +0800

    init

---
+++ pages/groupBuy/groupBuy.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="main">
+		拼团
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="main">
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+.main {
+		color: #DD524D;
+	}
+</style>
