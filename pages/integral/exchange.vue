6d7865a6e5bed914950dff5b5745b30e1848b640
zenglu <>
2020-07-25 14:44:30 +0800

    积分兑换

--- pages/integral/exchange.vue
+++ pages/integral/exchange.vue
@@ -1,11 +1,56 @@
 <template>
-	<view class="">
-		积分兑换
+	<view class="intergral-exchange-box">
+		<view class="header">
+			<view class="left">
+				<view class="title">
+					
+				</view>
+				<view class="content">
+					
+				</view>
+			</view>
+			<view class="right">
+				
+			</view>
+
+		</view>
 	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				moneys: ['999元', '1997元', '4998元', '9988元', '19888元', '其他'],
+				methods: ['支付宝', '微信'],
+				activeIndex: '',
+				methodIndex: ''
+			};
+		},
+		methods: {
+
+			changMoney(val, i) {
+				this.activeIndex = i
+				console.log(i)
+				console.log(val)
+			},
+
+			changMethod(val, i) {
+				this.methodIndex = i
+			},
+
+			submit() {
+				//发送请求
+			}
+		}
+	};
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.intergral-exchange-box {
+		.header{
+			height: 300rpx;
+			background: red;
+		}
+	}
 </style>
