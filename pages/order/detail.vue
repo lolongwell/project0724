ba56e61031a93c89792288a8c4e8840ad2c36fbe
lolongwell <lolong.well@qq.com>
2020-07-31 21:24:46 +0800

    支付

--- pages/order/detail.vue
+++ pages/order/detail.vue
@@ -41,129 +41,130 @@
 </template>
 
 <script>
-	import moneyAPI from '@/api/money/money.js';
-	export default {
-		data() {
-			return {
-				name: '333333eeeeeeeeeeeeee33',
-				detailList:{},
-				form:{
-					name:'33333333'
-				}
-			};
-		},
-		computed:{
-			orderDetails(){
-				return this.$store.state.orderDetails
-			}
-		},
-		onShow() {
-			this.detailList = this.orderDetails
-			console.log('aaa',this.detailList)
-		},
-		methods: {
-			payHandle() {
-          
-			 // let data = JSON.parse(JSON.stringify(this.form));
-			 // let o = {
-				//  userid:uni.getStorageSync('userid').,
-				//  openid:uni.getStorageSync('openid'),
-				//  zfje:'0.01'
-			 // }
-			 
-			 
-			 
-			 // 提现-提交
-			 // moneyAPI.inMoney(data).then(res => {
-			 // 	// this.$_log('提现：', res.data);
-			 // 	uni.showToast({
-			 // 		title: '提现成功!',
-			 // 		icon: 'none'
-			 // 	});
-			 // 	uni.navigateBack();
-			 // });
-			 // 支付成功
-			 console.log(this.form)
-			   uni.navigateTo({
-			   	url: `/pages/myMoney/paySuccess`
-			   });
-			}
-
-
-
-
+import moneyAPI from "@/api/money/money.js";
+import {requestPayOpenid, getUrlParam, payment} from '../../util'
+export default {
+  data() {
+    return {
+      name: "333333eeeeeeeeeeeeee33",
+			detailList: {},
+    };
+  },
+  computed: {
+    orderDetails() {
+      return this.$store.state.orderDetails;
+    },
+	},
+	onLoad() {
+		let local = window.location.href
+		console.log('订单详情', local)
+		let pay_openid = getUrlParam('openid')
+		if (pay_openid) {
+			console.log('pay_openid', pay_openid)
+			payment(pay_openid)
 		}
-	};
+	},
+  onShow() {
+    this.detailList = this.orderDetails;
+    console.log("aaa", this.detailList);
+  },
+  methods: {
+    payHandle() {
+			requestPayOpenid()
+
+
+
+      // let data = JSON.parse(JSON.stringify(this.form));
+      // let o = {
+      //  userid:uni.getStorageSync('userid').,
+      //  openid:uni.getStorageSync('openid'),
+      //  zfje:'0.01'
+      // }
+
+      // 提现-提交
+      // moneyAPI.inMoney(data).then(res => {
+      // 	// this.$_log('提现：', res.data);
+      // 	uni.showToast({
+      // 		title: '提现成功!',
+      // 		icon: 'none'
+      // 	});
+      // 	uni.navigateBack();
+      // });
+      // 支付成功
+      // console.log(this.form);
+      // uni.navigateTo({
+      //   url: `/pages/myMoney/paySuccess`,
+      // });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-	.order-detail-box {
-		padding: $page-row-spacing 0;
-		font-size: $font-base;
+.order-detail-box {
+  padding: $page-row-spacing 0;
+  font-size: $font-base;
 
-		ul {
-			padding: 0;
-			background: #fff;
+  ul {
+    padding: 0;
+    background: #fff;
 
-			li {
-				font-size: 30rpx;
-				display: flex;
-				// justify-content: space-around;
-				border-top: 1px solid #ddd;
-				padding: $page-row-spacing 0;
-				justify-content: space-between;
-				padding: 20rpx;
+    li {
+      font-size: 30rpx;
+      display: flex;
+      // justify-content: space-around;
+      border-top: 1px solid #ddd;
+      padding: $page-row-spacing 0;
+      justify-content: space-between;
+      padding: 20rpx;
 
-				span {
-					
-                    // flex: 1;
-					&:nth-of-type(1) {
-						flex: 1;
-					}
+      span {
+        // flex: 1;
+        &:nth-of-type(1) {
+          flex: 1;
+        }
 
-					&:nth-of-type(2){
-						flex: 3;
-					}
-				}
-			}
-		}
+        &:nth-of-type(2) {
+          flex: 3;
+        }
+      }
+    }
+  }
 
+  .pay-method {
+    margin-top: 30rpx;
 
-		.pay-method {
-			margin-top: 30rpx;
+    .pay-wechar {
+      margin-top: $page-row-spacing;
+      background: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0 30rpx;
 
-			.pay-wechar {
-				margin-top: $page-row-spacing;
-				background: #fff;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				padding: 0 30rpx;
+      .left {
+        flex: 1;
+      }
 
-				.left {
-					flex: 1;
-					
-				}
+      .center {
+        flex: 6;
+        display: flex;
+        flex-direction: column;
 
-				.center {
-					flex: 6;
-					display: flex;
-					flex-direction: column;
+        p {
+          padding: 10rpx;
 
-					p {
-						padding: 10rpx;
+          &:nth-of-type(2) {
+            color: #999;
+          }
+        }
+      }
 
-						&:nth-of-type(2) {
-							color: #999;
-						}
-					}
-				}
-
-				.right {
-					flex: 1;
-					text-align: right;
-				}
-			}
-		}
-	}
+      .right {
+        flex: 1;
+        text-align: right;
+      }
+    }
+  }
+}
 </style>
--- util.js
+++ util.js
@@ -1,8 +1,10 @@
-import RegisterAPI from "@/api/register/register";
+import RegisterAPI from '@/api/register/register'
+import payApi from '@/api/pay/pay.js'
 
 export function getUrlParam(name) {
   var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
-  let url = window.location.href.split('#')[0]
+  // let url = window.location.href.split('#')[0]
+  let url = window.location.href
   let search = url.split('?')[1]
   if (search) {
     var r = search.substr(0).match(reg)
@@ -14,14 +16,16 @@ export function getUrlParam(name) {
 }
 
 export function checkLogin() {
-  const appId = getApp().globalData.appId;
-  const local = window.location.href;
-  let code = getUrlParam("code");
-  console.log("code", code);
-  if (code === null || code === "") {
-    window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURIComponent(local)}&response_type=code&scope=snsapi_userinfo#wechat_redirect`;
+  const appId = getApp().globalData.appId
+  const local = window.location.href
+  let code = getUrlParam('code')
+  console.log('code', code)
+  if (code === null || code === '') {
+    window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURIComponent(
+      local
+    )}&response_type=code&scope=snsapi_userinfo#wechat_redirect`
   } else {
-    console.log("code已存在", code);
+    console.log('code已存在', code)
     getWxInfo(code)
   }
 }
@@ -31,25 +35,78 @@ export function getWxInfo(code) {
     RegisterAPI.getOpenId(code).then((res) => {
       if (res.data.respCode == 0) {
         let session_key = res.data.data.session_key || ''
-        uni.setStorageSync("openid", res.data.data.openid);
-        uni.setStorageSync("avatarUrl", res.data.data.avatarUrl);
-        uni.setStorageSync("city", res.data.data.citizenNo);
-        uni.setStorageSync("province", res.data.data.province);
-        uni.setStorageSync("country", res.data.data.country);
-        uni.setStorageSync("nickName", res.data.data.nickName);
-        uni.setStorageSync("unionid", res.data.data.unionid);
-        uni.setStorageSync("sex", res.data.data.sex);
-        if (session_key) {  // 注册流程
+        uni.setStorageSync('openid', res.data.data.openid)
+        uni.setStorageSync('avatarUrl', res.data.data.avatarUrl)
+        uni.setStorageSync('city', res.data.data.citizenNo)
+        uni.setStorageSync('province', res.data.data.province)
+        uni.setStorageSync('country', res.data.data.country)
+        uni.setStorageSync('nickName', res.data.data.nickName)
+        uni.setStorageSync('unionid', res.data.data.unionid)
+        uni.setStorageSync('sex', res.data.data.sex)
+        if (session_key) {
+          // 注册流程
           console.log('要注册')
-          uni.setStorageSync("session_key", res.data.data.session_key);
+          uni.setStorageSync('session_key', res.data.data.session_key)
           reject()
-        } else {    // 用户已注册，获取token及用户信息
+        } else {
+          // 用户已注册，获取token及用户信息
           console.log('要登录')
-          uni.setStorageSync("TOKEN", res.data.data.token);
-          uni.setStorageSync("userid", res.data.data.id);
+          uni.setStorageSync('TOKEN', res.data.data.token)
+          uni.setStorageSync('userid', res.data.data.id)
           resolve()
         }
       }
-    });
+    })
   })
+}
+
+// 获取支付openid
+export function requestPayOpenid() {
+  let local = window.location.href
+  window.location.href = `https://api.yiyunjf.cn/v1/wechat/auth?app_id=pya7LZA0fnMupQH5Yp4C9wZtMDAwV1RP&sub_mch_id=1601407358&snsapi_userinfo=true&callback=${encodeURIComponent(
+    local
+  )}`
+}
+
+export function payment(openid) {
+  let o = {
+    userid: uni.getStorageSync('userid'),
+    openid,
+    czje: '0.01',
+  }
+  payApi.payOrder(o).then((res) => {
+    console.log('支付返回信息', res)
+    if (res.data.data.code == 1000) {
+      let data = res.data.data.data
+      if (typeof WeixinJSBridge == 'undefined') {
+        if (document.addEventListener) {
+          document.addEventListener('WeixinJSBridgeReady', onBridgeReady.bind(null,data), false)
+        } else if (document.attachEvent) {
+          document.attachEvent('WeixinJSBridgeReady', onBridgeReady.bind(null,data))
+          document.attachEvent('onWeixinJSBridgeReady', onBridgeReady.bind(null,data))
+        }
+      } else {
+        onBridgeReady(data)
+      }
+    }
+  })
+}
+
+export function onBridgeReady(data) {
+  WeixinJSBridge.invoke(
+    'getBrandWCPayRequest',
+    {
+      appId: data.jsapi_app_id, //公众号名称，由商户传入
+      timeStamp: data.jsapi_timeStamp, //时间戳，自1970年以来的秒数
+      nonceStr:  data.jsapi_nonceStr, //随机串
+      package: data.jsapi_package,
+      signType: 'MD5',
+      paySign: data.jsapi_paySign, //微信签名
+    },
+    function (res) {
+      if (res.err_msg == 'get_brand_wcpay_request:ok') {
+        alert('支付成功')
+      }
+    }
+  )
 }
